41e5cd7bd6a2f4e6d7343bc792b7b141af7cd781
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-03-19 03:03:36 +0100

    add testcase

--- rtc-app/db/sqlc/users_test.go
+++ rtc-app/db/sqlc/users_test.go
@@ -3,13 +3,14 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/ihi108/hello-rtc/rtc-app/util"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUser(t *testing.T) {
+func RandomUser(t *testing.T) User {
 	arg := CreateUserParams{
 		Username:       util.RandomName(7),
 		FirstName:      util.RandomName(8),
@@ -33,4 +34,32 @@ func TestCreateUser(t *testing.T) {
 	require.NotZero(t, user.ID)
 	require.NotZero(t, user.CreatedAt)
 	require.NotZero(t, user.UpdatedAt)
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	RandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := RandomUser(t)
+
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.FirstName, user2.FirstName)
+	require.Equal(t, user1.LastName, user2.LastName)
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.DateOfBirth.Year(), user2.DateOfBirth.Year())
+	require.Equal(t, user1.DateOfBirth.Month(), user2.DateOfBirth.Month())
+	require.Equal(t, user1.DateOfBirth.Day(), user2.DateOfBirth.Day())
+	require.Equal(t, user1.Username, user2.Username)
+
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+	require.WithinDuration(t, user1.UpdatedAt, user2.UpdatedAt, time.Second)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
 }
