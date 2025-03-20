be57bc7367f4740a308d8a87200e30205349179e
ifeanyichukwu <ifeanyichukwuhillary2019@gmail.com>
2025-03-20 20:09:14 +0100

    add user registration function

--- rtc-app/server/user.go
+++ rtc-app/server/user.go
@@ -1,20 +1,23 @@
 package server
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	db "github.com/ihi108/hello-rtc/rtc-app/db/sqlc"
 	"github.com/ihi108/hello-rtc/rtc-app/util"
 )
 
-type createUserFormRequest struct {
+type loginUserFormRequest struct {
 	Username string `form:"username" binding:"required,alphanum"` // no special characters in username
 	Password string `form:"password" binding:"required,min=6"`
 }
 
 func (server *Server) loginUser(ctx *gin.Context) {
-	var userData createUserFormRequest
+	var userData loginUserFormRequest
 
 	session := sessions.Default(ctx)
 	meet_id := session.Get("meet_id")
@@ -46,5 +49,56 @@ func (server *Server) loginUser(ctx *gin.Context) {
 	}
 }
 
+type createUserFormRequest struct {
+	Username    string    `form:"username"`
+	FirstName   string    `form:"first_name"`
+	LastName    string    `form:"last_name"`
+	MiddleName  string    `form:"middle_name"`
+	Email       string    `form:"email"`
+	Password    string    `form:"password"`
+	DateOfBirth time.Time `form:"date_of_birth"`
+}
+
 func (server *Server) signupUser(ctx *gin.Context) {
+	var userFormData createUserFormRequest
+
+	session := sessions.Default(ctx)
+	meet_id := session.Get("meet_id")
+
+	if err := ctx.ShouldBind(&userFormData); err != nil {
+		ctx.HTML(http.StatusUnauthorized, "register.html", errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.GeneratePasswordHash(userFormData.Password)
+	if err != nil {
+		ctx.HTML(http.StatusInternalServerError, "register.html", errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       userFormData.Username,
+		FirstName:      userFormData.FirstName,
+		LastName:       userFormData.LastName,
+		MiddleName:     sql.NullString{String: userFormData.MiddleName, Valid: true},
+		Email:          userFormData.Email,
+		HashedPassword: hashedPassword,
+		DateOfBirth:    userFormData.DateOfBirth,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		ctx.HTML(http.StatusUnauthorized, "login.html", errorResponse(err))
+		return
+	}
+
+	// save the username in the session with the userData(username & password)
+	session.Set("user", user)
+	session.Save()
+
+	if meet_id == nil {
+		ctx.Redirect(http.StatusFound, "/apps")
+	} else {
+		ctx.Redirect(http.StatusFound, "/meet/"+meet_id.(string))
+	}
 }
